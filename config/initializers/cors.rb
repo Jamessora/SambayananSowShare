e9795a406bbee776ec2aea7483f41935cc0e549d
Jamessora <cadaciojerico@gmail.com>
2023-10-21 13:59:42 +0800

    Reverted to commit 7b071949fc85470ae6d4d3acc3a1dbc9e270b2d7

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://sambayanansowsharefe.onrender.com'
+     origins 'http://localhost:5173', 'http://127.0.0.1:5173', https://sambayanansowsharefe.onrender.com,
 
     resource "*",
       headers: :any,
